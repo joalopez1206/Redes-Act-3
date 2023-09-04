86eb375925978cd98cabda020799832dd67c22ca
joalopez1206 <joalopez@ug.uchile.cl>
2023-09-04 00:42:06 -0300

    changed the structure for the resolver

--- src/resolver.py
+++ src/resolver.py
@@ -1,11 +1,23 @@
 import socket
-SIZE = 4096
+from dns_parse_mod import parse_dns_message
+from utils import send_dns_msg, has_type_a_in_section_ans, SIZE
 
+ROOT_SV = ("192.33.4.12", 53)
 local_addr = ('localhost', 8000)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(local_addr)
 
+
+def resolver(consulta: bytes, loc_addr=ROOT_SV):
+    # Enviamos la query al servidor raiz y esperamos la consulta
+    answer: bytes = send_dns_msg(sock, loc_addr, consulta)
+    # Parseamos la respuesta
+    parsed_answer = parse_dns_message(answer)
+    if has_type_a_in_section_ans(parsed_answer):
+        return answer
+
+
 while True:
     data, a = sock.recvfrom(SIZE)
-    print(a)
-    print(data)
+    reply = resolver(data)
+    sock.sendto(reply, a)
