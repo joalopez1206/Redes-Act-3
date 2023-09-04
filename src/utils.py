48e0ece1c32fe21d5379776a6a4f78c5b8af1f10
joalopez1206 <joalopez@ug.uchile.cl>
2023-09-04 01:45:20 -0300

    added somethings

--- src/utils.py
+++ src/utils.py
@@ -6,10 +6,21 @@ SIZE = 4096
 
 def has_type_a_in_section_ans(parsed_answer: DNSRecord):
     print(f"parsed rr answers: {parsed_answer.rr}")
-    return True
+    print(f"parsed rr authority: {parsed_answer.auth}")
+    print(f"parsed rr header: {parsed_answer.header}")
+    # print(f"parsed rr a : {parsed_answer.a}")
+    # Todo: find a lambda that says if there is atleast one
+    # any(map(lambda x: x.,parsed_answer.a))
+    return parsed_answer.header.a > 0
 
 
-def send_dns_msg(sock_s: socket.socket, addr: tuple[str, int], msg: bytes):
+def has_type_ns_in_section_auth(parsed_answer: DNSRecord):
+    print(f"parsed ar {parsed_answer.ar}")
+    return parsed_answer.header.ar > 0
+
+
+def send_dns_request(addr: tuple[str, int], msg: bytes):
+    sock_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         sock_s.sendto(msg, addr)
         data, _ = sock_s.recvfrom(4096)
